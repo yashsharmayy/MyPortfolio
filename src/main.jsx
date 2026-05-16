1f06b4fc4f6d362f5ac0a222ce3b78b3c6f0fb9c
yash sharma <ys4468570@gmail.com>
2026-05-17 01:53:10 +0530

    feat: switch from BrowserRouter to HashRouter for routing in main component

--- src/main.jsx
+++ src/main.jsx
@@ -3,13 +3,13 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { HoverProvider } from './components/Context.jsx'
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter, HashRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
 
   <HoverProvider>
-    <BrowserRouter basename='MyPortfolio'>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </HoverProvider>
 )
